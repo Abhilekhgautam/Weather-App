b4ee093842af026985f76c7ca577ccc421d85d38
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2021-02-22 13:38:44 +0545

    minor updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const process = () =>{
     if(document.querySelector('.inputBox').value=="")
     {
         document.querySelector('.warning').innerText="Input  field cannot be empty"
-        document.querySelector('.warning').style.color="white"
+        document.querySelector('.warning').style.color="cyan"
     }
     else{
         var input =document.querySelector('.inputBox').value
@@ -11,16 +11,25 @@ const process = () =>{
         .then(res=>{
             if(res.status===400){
                 document.querySelector('.warning').innerText="No data found"
-               document.querySelector('.warning').style.color="white"
+               document.querySelector('.warning').style.color="cyan"
                document.querySelector('.weatherStatus').innerText=""
                document.querySelector('.imageStatus').innerHTML=""
+               document.querySelector('.currentTemp').innerText=""
+              document.querySelector('.lastUpdated').innerText=""
                return res
             }
             return res.json()
         })
         .then((location)=>{
+            console.log(location)
             document.querySelector('.weatherStatus').innerText=`${location.current.condition.text} at ${input}`
             document.querySelector('.imageStatus').innerHTML=`<img src=${location.current.condition.icon} alt="Weather Icon">`
+            document.querySelector('.currentTemp').innerText=`Current Temperature:${location.current.temp_c} C`
+            document.querySelector('.currentTemp').style.color="red"
+            document.querySelector('.lastUpdated').innerText=`Last updated at:${location.current.last_updated}`
+            document.querySelector('.lastUpdated').style.color="red"
+
+
         })
         .catch((err)=>{
         throw err
